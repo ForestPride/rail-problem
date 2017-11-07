ce490c722c34a73b38e42053a753c75f5c9e8aa6
amberhunt <amber@marlboro.edu>
2017-11-07 09:46:58 -0500

    Create train class
    Ref #4

---
+++ train.py
@@ -0,0 +1,19 @@
+"""Creates the train class"""
+
+
+class Train:
+    """
+    Each train in the metro is an instance of the Train class.
+    Methods:
+    __init__: creates a new train in the station
+    BeforeTrain: runs a Monte Carlo simulation before train arrives
+    InStation: runs a Monte Carlo simluation while train is in station
+    AfterTrain: runs a Monte Carlo simluation after train departure
+    """
+
+    def __init__(self,):
+        #self.numberofcars = numberofcars
+        #self.numberofpeopleincar = numberofpeopleincar
+        #self.boardrate = boardrate
+        #self.arrivaltime = arrivaltime
+        #self.dwelltime = dwelltime
