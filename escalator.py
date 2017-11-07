39dde9ec30793a49165f23a9d6cfebba46a09409
amberhunt <amber@marlboro.edu>
2017-11-07 14:34:44 -0500

    Add escalator class properties
    Ref #11

---
+++ escalator.py
@@ -0,0 +1,13 @@
+"""Creates the escalator class"""
+
+
+class Escalator:
+    """
+    Each escalator is an instance of the escalator class.
+    Methods:
+    __init__: creates a new timetoescalator
+    """
+
+    def __init__(self,):
+        #self.standingtransittime = standingtransittime
+        #self.walkingtransittime = walkingtransittime
