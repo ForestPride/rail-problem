cdae77dee9888d6d6094566747650bf80d631f03
amberhunt <31616988+amberhunt@users.noreply.github.com>
2017-11-30 09:35:30 -0500

    Integrate integer test function into instantiation
    Ref #23

--- station.py
+++ station.py
@@ -2,9 +2,8 @@
 
 
 
-#import ask_user from ask_user
-#import int_check from int_check
-#import reasonable_check from reasonable_check
+#import request_integer_in_range from request_integer_in_range
+
 
 
 class Station:
@@ -12,14 +11,13 @@ class Station:
     Each train station is an instance of the Station class.
     Methods:
     __init__: creates a new stations
-    total_station_pop: calculates total station population
-    ask_user(prompt, lower_range, upper_range): function to get input, maybe it should live
-    somewhere else?
+    request_integer_in_range : requests an integer in a range
+    
     """
 
     def __init__(self, capacity, escalators, train_wait, travelors_arriving, travelors_departing):
-        self.capacity = user.says("Enter the max capacity of the station between" lower "and" upper)
-        self.escalators = user.says("Enter the number of escalators in the station between" lower "and" upper)
-        self.train_wait = user.says("Enter the wait time between trains in seconds between" lower "and" upper)
-        self.travelors_arriving = user.says("How many people just exited the train? between" lower "and" upper)
-        self.travelors_departing = user.says("How many people are waiting for the train? between" lower "and" upper)
+        self.capacity = request_integer_in_range("Enter the station capacity between 10 and 10000: ", 10, 10000)
+        self.escalators = request_integer_in_range("Enter an odd number of escalators between 1 and 7: ", 1, 7)
+        self.train_wait = request_integer_in_range("Enter the wait time between trains in seconds between 60 and 1800 ", 60, 1800)
+        self.travelors_arriving = request_integer_in_range("Enter the number of people exiting the train between 1 and 500: ", 1, 500)
+        self.travelors_departing = request_integer_in_range("Enter the number of people waiting for the train between 1 and 500: ", 1, 500)
