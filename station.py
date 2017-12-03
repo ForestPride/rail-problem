12c7d058639f55f3d8dc8428e99b68720a21032c
ForestPride <npride@marlboro.edu>
2017-12-02 22:46:01 -0500

    Update station class
    Fixed some of the language in the station class.
    
    ref #10

--- station.py
+++ station.py
@@ -1,9 +1,7 @@
 """Creates the station class"""
 
 
-from request_integer_in_range import *
-
-
+from request_integer_in_range import request_integer_in_range
 
 class Station:
     """
@@ -18,10 +16,20 @@ class Station:
         self.capacity = request_integer_in_range("Enter the station capacity between 10 and 10000: ", 10, 10000)
         self.escalators = request_integer_in_range("Enter an odd number of escalators between 1 and 7: ", 1, 7)
         self.train_wait = request_integer_in_range("Enter the wait time between trains in seconds between 60 and 1800 ", 60, 1800)
-        self.travelors_arriving = request_integer_in_range("Enter the number of people exiting the train between 1 and 500: ", 1, 500)
-        self.travelors_departing = request_integer_in_range("Enter the number of people waiting for the train between 1 and 500: ", 1, 500)
+        self.travelers_arriving = request_integer_in_range("Enter the number of people that exited the train between 1 and 500: ", 1, 500)
+        self.travelers_departing = request_integer_in_range("Enter the number of people waiting for the train between 1 and 500: ", 1, 500)
 
     @property
     def pop(self):
         """Computes the total population of the station."""
-        return self.travelors_arriving + self.travelors_departing
+        return self.travelers_arriving + self.travelers_departing
+
+    @property
+    def escalators_entering(self):
+        """compute the number of escalators leaving the station."""
+        return (self.escalators // 2) + 1
+
+    @property
+    def escalators_exiting(self):
+        """compute the number of escalators exiting the station."""
+        return self.escalators // 2
