3d1612e5f9e20cf74a962dd4ca1b538776d5ec7e
ForestPride <npride@marlboro.edu>
2017-12-02 22:52:33 -0500

    Simplify the function to calculate the platform population between trains
    The function to calculate the change in platform population in the time
    between trains was needlessly complex. It has now been simplified.
    
    ref #17

--- StationPopWithoutTrain.py
+++ StationPopWithoutTrain.py
@@ -1,8 +1,10 @@
+"""This module calculates the number of people in the station by the time the next train arives"""
+
 def before_train_station_pop(station, escalator):
-    # calculate the number of people waiting to depart on the train by the time the train arive.
-    station.travelers_departing = station.travelers_departing + (escalator.rate * escalators.entering * station.train_wait)
+    """This function calculates the total number of people as a sume of people
+    waiting to board the next train, and the number of people waiting to leave
+    the station by the elebvators."""
+
+    station.travelers_departing = station.travelers_departing + (escalator.rate * station.escalators_entering * station.train_wait)
     # number of people who have arived and want to leave.
-    station.travelers_arriving = station.travelers_arriving - (escalator.rate * station.train_time
-    # Get the total station population.
-    population = station.pop
-    return population
+    station.travelers_arriving = station.travelers_arriving - (escalator.rate * station.escalators_exiting * station.train_wait)
