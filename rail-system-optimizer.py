7bbc88f4036bcbd82b5f5135b45732f1cc510f21
ForestPride <npride@marlboro.edu>
2017-12-02 22:54:46 -0500

    Bug fixes for the main function
    ref #17

--- rail-system-optimizer.py
+++ rail-system-optimizer.py
@@ -5,43 +5,49 @@ This program optimizes the efficiency of a train station by simulating normal
 operations and varying train schedules and escalator ediquette.
 """
 
-from StationPopWithoutTrain import before_train_station_pop
-from StationPopWithTrain import durring_train_station_pop
 from MonteCarloSim import simulation
 from station import Station
 from train import Train
 from escalator import Escalator
 
-runs = eval(input("Enter the number of simulations you want to run: "))
-failure_threshold = (eval(input("Enter acceptable system failure percentage: ")) //
-                     100) * runs
+RUNS = int(input("Enter the number of simulations you want to run: "))
+FAILURE_TRESHOLD = (int(input("Enter acceptable system failure percentage: ")) // 100) * RUNS
 
 def main():
+    print("create a train station: enter the appropriate values for your station.")
     station_1 = Station()
     # Create a new station that is the same as station_1 before runnning the simulation.
-    print(station_1.capacity, "is the station capacity")
     station_2 = station_1
+    print("Create a train: enter the appropriate values for your trains.")
     train_1 = Train()
     # Create a standing only escalator.
+    print("Create a standing only escalator: enter the data for the behavior you have observed.")
     escalator_1 = Escalator()
     # Create a standing and walking escalator.
+    print("Create a standing and walking escalator: enter the data for the behavio you have observed.")
     escalator_2 = Escalator()
 
-    for i in range(runs):
+    for i in range(RUNS):
+        print(station_1.pop)
         while station_1.train_wait != 3600:
-            failures = simulation(train_1, station_1, escalator_1)[0]
+            failures = simulation(station_1, train_1, escalator_1)[0]
             print(failures)
-            if failures >= failure_threshold:
+            if failures >= FAILURE_TRESHOLD:
                 station_1.train_wait = station_1.train_wait + 60
             else:
                 pass
+        i = i
 
-    for i in range(runs):
+    for i in range(RUNS):
         while station_1.train_wait != 3600:
-            failures = simulation(train_1, station_2, escalator_2)[0]
+            failures = simulation(station_2, train_1, escalator_2)[0]
             print(failures)
-            if failures >= failure_threshold:
+            if failures >= FAILURE_TRESHOLD:
                 station_2.train_wait = station_2.train_wait + 60
             else:
                 pass
+        i = i
+
+    print("the time between trains for the station with standing only escalators is: ", station_1.train_wait)
+    print("The time between trains for the station with standing and walking escalators is: ", station_2.train_wait)
 main()
