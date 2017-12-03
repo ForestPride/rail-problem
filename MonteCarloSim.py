6cee36e5286d219e7af27a08cf1cc833c7922301
ForestPride <npride@marlboro.edu>
2017-12-02 22:56:19 -0500

    Bug fixes for the Monte Carlo simulation
    Some bugs in the simulation have been fixed, and some of the
    calculations have been moved from supporting functions into this
    function to restructure the code in a more elegant way.
    
    ref #17

--- MonteCarloSim.py
+++ MonteCarloSim.py
@@ -1,25 +1,58 @@
 
 from StationPopWithoutTrain import before_train_station_pop
-from StationPopWithTrain import durring_train_station_pop
+from StationPopWithTrain import train_unloading_station_pop, train_boarding_station_pop
+from random import random
 
 def simulation(station, train, escalator):
+    # Generate a random number to set the size of the train.
+    dice_roll = random()
+    # The number of cars the train has.
+    if  dice_roll <= 0.30:
+        train.cars = 8
+    elif 0.30 < dice_roll <= 0.60:
+        train.cars = 10
+    else:
+        train.cars = 12
+    print("the number of train cars is: ", train.cars)
+
+    # The number of people on the train.
+    train.pop = int(dice_roll * train.car_capacity * train.cars)
+    print("the populatio of the train is: ", train.pop)
+
+    """
+    The number of people that want to get off the train.
+    There is a new random number here so that the percentage of people leaving
+    the train does not depend on the population
+    """
+    travelers_exiting_train = int(train.pop * random())
+    print("the number of travelers exiting the train is: ", travelers_exiting_train)
+
     overflows = 0
     for i in range(10):
         # Let time pass with no train in the station.
-        population = before_train_station_pop(station, escalator_standing)
+        before_train_station_pop(station, escalator)
+        print("the station population is: ", station.pop)
         # check to see if the sation overflows.
-        if population >= station.capacity:
+        if station.pop >= station.capacity:
             overflows = overflows + 1
+            print(overflows)
 
-        # Calculate the station population second by second while the train is at the sation.
-        # Starting a clock when the train arrives.
+        """
+        Calculate the station population second by second while the train is at the sation.
+        Starting a clock when the train arrives.
+        """
         dwell_time = 0
-        while station.departing != 0 or Train.pop != train.cap:
-            pupulation = population + durring_train_station_pop(station, train, escalator_standing)
+        while travelers_exiting_train != 0:
+            train_unloading_station_pop(station, train, escalator)
+            print("the station population is: ", station.pop)
             # Advance the clock one second.
             dwell_time = dwell_time + 1
             # Check to see if the station overflows.
-            if population >= station.capacity:
+            if station.pop >= station.capacity:
                 overflows = overflows + 1
+                print(overflows)
 
+        while station.travelers_departing != 0 or train.pop != train.capacity:
+            train_boarding_station_pop(station, train, escalator)
+        i = i
     return overflows, dwell_time
