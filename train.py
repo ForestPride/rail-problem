e57b61ea05a505f96ddc4317db7ac9c727eb32ab
ForestPride <npride@marlboro.edu>
2017-12-02 22:44:01 -0500

    Update train class
    The train class was asking the user to input some values that are in
    reality set by the program.
    
    ref #9

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 """Creates the train class"""
 
-
-from request_integer_in_range import *
-
 class Train:
     """
     Each train in the metro is an instance of the Train class.
@@ -11,8 +8,12 @@ class Train:
     """
 
     def __init__(self):
-        self.cars = request_integer_in_range("Enter even number of train cars between 8-12: ", 8, 12)
+        self.cars = None
         self.board_rate = 8
-        self.population = population
+        self.pop = None
         self.dwell_time = None
         self.car_capacity = 125
+
+    @property
+    def cap(self):
+        return self.cars * self.car_capacity
